f768204bcd173aafaac493ec2a6018757b456a9f
DESKTOP-P7KE4DS\phant <vuduccuong.ck48@gmail.com>
2018-07-19 17:51:58 +0700

    Add DataModel

---
+++ Management.Model/Models/Footer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Management.Model.Models
+{
+    [Table("Footers")]
+    public class Footer
+    {
+        [Key]
+        [MaxLength(50)]
+        public string ID { set; get; }
+
+        [Required]
+        public string Content { set; get; }
+    }
+
+}
---
+++ Management.Model/Models/MenuGroup.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Management.Model.Models
+{
+    [Table("MenuGroups")]
+    class MenuGroup
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { set; get; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { set; get; }
+
+        public virtual IEnumerable<Menu> Menus { set; get; }
+
+    }
+}
