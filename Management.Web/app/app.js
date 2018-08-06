0b2f4a6e2ea36eb99d75f0c6d4db01ace453dd7e
DESKTOP-P7KE4DS\phant <vuduccuong.ck48@gmail.com>
2018-08-07 04:01:04 +0700

    Tạo cấu trúc thư mục app và làm ví dụ cho controller trong angularjs

---
+++ Management.Web/app/app.js
@@ -0,0 +1,16 @@
+﻿/// <reference path="../scripts/angular.js" />
+
+(function () {
+    angular.module('management', ['management.products', 'management.common']).config(config);
+
+    config.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+    function config($stateProvider, $urlRouterProvider) {
+        $stateProvider.state('home', {
+            url: "/admin",
+            templateUrl: "/app/components/home/homeView.html",
+            controller: "homeController"
+        });
+        $urlRouterProvider.otherwise('/admin');
+    }
+})();
