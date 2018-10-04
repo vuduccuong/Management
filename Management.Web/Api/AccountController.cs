02cb8300aeaa130138fa96a7309b5369e9ab4e5c
DESKTOP-P7KE4DS\phant <vuduccuong.ck48@gmail.com>
2018-10-04 23:57:26 +0700

    Login admin save info to local stogare

--- Management.Web/Api/AccountController.cs
+++ Management.Web/Api/AccountController.cs
@@ -62,5 +62,15 @@ namespace Management.Web.Api
             var result = await SignInManager.PasswordSignInAsync(userName, password, rememberMe, shouldLockout: false);
             return request.CreateResponse(HttpStatusCode.OK, result);
         }
+
+        [HttpPost]
+        [Authorize]
+        [Route("logout")]
+        public HttpResponseMessage Logout(HttpRequestMessage request)
+        {
+            var authenticationManager = HttpContext.Current.GetOwinContext().Authentication;
+            authenticationManager.SignOut();
+            return request.CreateResponse(HttpStatusCode.OK, new { success = true });
+        }
     }
 }
--- Management.Web/app/shared/services/apiService.js
+++ Management.Web/app/shared/services/apiService.js
@@ -3,55 +3,17 @@
 (function (app) {
     app.factory('apiService', apiService);
 
-    apiService.$inject = ['$http'];
+    apiService.$inject = ['$http', 'notificationService', 'authenticationService'];
 
-    function apiService($http) {
+    function apiService($http, notificationService, authenticationService) {
         return {
             get: get,
             post: post,
             put: put,
             del: del
-        };
-
-        function get(url, params, success, failure) {
-            $http.get(url, params).then(function (result) {
-                success(result);
-            }, function (error) {
-                failure(error);
-            });
         }
-
-        function post(url, data, success, failure) {
-            $http.post(url, data).then(function (result) {
-                success(result);
-            }, function (error) {
-                console.log(error.status);
-                if (error.status === 401) {
-                    notificationService.displayError('Authenticate is required.');
-                }
-                else if (failure !== null) {
-                    failure(error);
-                }
-
-            });
-        }
-
-        function put(url, data, success, failure) {
-            $http.put(url, data).then(function (result) {
-                success(result);
-            }, function (error) {
-                console.log(error.status);
-                if (error.status === 401) {
-                    notificationService.displayError('Authenticate is required.');
-                }
-                else if (failure !== null) {
-                    failure(error);
-                }
-
-            });
-        }
-
         function del(url, data, success, failure) {
+            authenticationService.setHeader();
             $http.delete(url, data).then(function (result) {
                 success(result);
             }, function (error) {
@@ -65,6 +27,43 @@
 
             });
         }
+        function post(url, data, success, failure) {
+            authenticationService.setHeader();
+            $http.post(url, data).then(function (result) {
+                success(result);
+            }, function (error) {
+                console.log(error.status)
+                if (error.status === 401) {
+                    notificationService.displayError('Authenticate is required.');
+                }
+                else if (failure !== null) {
+                    failure(error);
+                }
 
+            });
+        }
+        function put(url, data, success, failure) {
+            authenticationService.setHeader();
+            $http.put(url, data).then(function (result) {
+                success(result);
+            }, function (error) {
+                console.log(error.status)
+                if (error.status === 401) {
+                    notificationService.displayError('Authenticate is required.');
+                }
+                else if (failure !== null) {
+                    failure(error);
+                }
+
+            });
+        }
+        function get(url, params, success, failure) {
+            authenticationService.setHeader();
+            $http.get(url, params).then(function (result) {
+                success(result);
+            }, function (error) {
+                failure(error);
+            });
+        }
     }
 })(angular.module('management.common'));
